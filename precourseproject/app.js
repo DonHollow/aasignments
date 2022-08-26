7d6c531e9ba32f769675b30b4da859e4c9a67ca8
Yreish Pozo <yreishmiguel@gmail.com>
2022-08-25 17:41:25 -0700

    Added precourseproject level 0

---
+++ precourseproject/app.js
@@ -0,0 +1,43 @@
+//Object
+const yreish = {
+  firstName: "Yreish",
+  lastName: "Pozo",
+  age: 22,
+  email: "yreishmiguel@gmail.com"
+}
+
+//Object
+let stuff = {
+  location:"puerto rico",
+  population: 1000000,
+  neighbors: ['cuba','dominican republic']
+}
+
+console.log(stuff.neighbors)
+
+//Random Color Array
+let colors = ["Black","Red","Yellow","Purple","Green"]
+
+//if-else simple statement
+if (book === "red blue") {
+  console.log("this is book")
+
+} else if (book === "red blue book") {
+  console.log("this is blue")
+} else {
+  console.log("this is red")
+}
+
+//event listener
+let button = document.getElementById("button")
+function handleButtonClick(){
+  console.log("The button was clicked!")
+}
+
+button.addEventListener("click", handleButtonClick)
+
+//loop
+for (let i = 0; i < 10; i++) {
+console.log(i)
+  
+}
