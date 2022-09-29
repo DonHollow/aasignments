1d3497ad353578118f1184f20fd3ee56dfbcdc93
Yreish Pozo <yreishmiguel@gmail.com>
2022-09-28 18:21:30 -0700

    Progress

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,18 +1,58 @@
-const userInput = document.getElementById("submitItem")
+const form = document.addItem
 
-document.addItem.addEventListener("submit", function(e){
+form.addEventListener("submit", function(e){
   e.preventDefault()
   
-  console.log(e.target[0].value)
-})
+  let list = document.getElementById("list")
+  
+  const input = form.title.value
+  
+  let newDiv = document.createElement("div")
+  
+  let listItem = document.createElement("li")
 
+  listItem.textContent = input
+  list.append(newDiv)
 
-userInput.addEventListener('onclick()', function(e){
-  var list = document.getElementById("newItem");
-  var deleteButton = document.getElementById("delete");
-  var addItem = document.createElement("li");
-  addItem.setAttribute("id", "item" + i + "");
-  addItem.innerHTML = 'Item ' + i;
-  deleteButton.setAttribute("onclick", "remove(this)");
-  list.appendChild(addItem);
+  newDiv.append(listItem)
+
+  form.title.value = ""
+
+  let edit = document.createElement("button")
+
+  edit.textContent = "edit"
+
+  let remove = document.createElement("button")
+  remove.textContent  = "X"
+
+  newDiv.append(edit, remove)
+
+  remove.setAttribute("class", "button")
+  edit.setAttribute("class","button1")
+  newDiv.setAttribute("class", "button")
+
+  remove.addEventListener("click", function(){
+    let listItem = this.parentNode;
+      let list = listItem.parentNode;
+      list.removeChild(listItem);
+  })
+
+  let saveBtn = document.createElement('button')
+  
+  let inputBox = document.createElement('input')
+
+  saveBtn.textContent = "Save"
+
+  edit.addEventListener("click", function(){
+    newDiv.append(inputBox)
+    inputBox.value = listItem.textContent
+    newDiv.append(saveBtn)
+  })
+
+  saveBtn.addEventListener("click", function(){
+    listItem.textContent = inputBox.value
+    inputBox.remove()
+    saveBtn.remove()
+  })
+
 })
