12503267114678ff6cd95a9f115f4c258b4b9d52
Yreish Pozo <yreishmiguel@gmail.com>
2022-09-30 21:18:29 -0700

    Progress

--- node-calc/calc.js
+++ node-calc/calc.js
@@ -1,13 +1,39 @@
-const readLineSync = require('readline-sync');
+const readlineSync = require('readline-sync');
 
-let num1 = readLineSync.keyIn('What is your first number? ');
-console.log(num1);
+let num1 = readlineSync.keyIn('What is your first number? ');
 
-let num2 = readLineSync.keyIn('What is your second number? ');
-console.log(num2);
+let num2 = readlineSync.keyIn('What is your second number? ');
 
-const operator = readLineSync.keyIn('Do you want to (a)dd, (s)ubstract, (d)ivide, or (m)ultiply? ', {
-  if (add) {
-    console.log(add(num1 + num2));
-  }
-});
+let add = Number(num1) + Number(num2)
+let sub = Number(num1) - Number(num2)
+let div = Number(num1) / Number(num2)
+let mul = Number(num1) * Number(num2)
+
+readlineSync.setDefaultOptions({limit: ['a', 's', 'm', 'd']});
+
+let operator = readlineSync.keyIn('Do you want to (a)dd, (s)ubstract, (d)ivide, or (m)ultiply? ');
+
+
+switch(operator){
+  case 'a':
+    console.log(add)
+    break;
+  case 's':
+    console.log(sub)
+    break;
+  case 'd':
+    console.log(div)
+    break;
+  case 'm':
+    console.log(mul)
+    break;
+}
+    // if (operator === 'a'){
+    //   console.log(add)
+    // } else if (operator === 's'){
+    //   console.log(sub)
+    // } else if (operator === 'd'){
+    //   console.log(div)
+    // } else if (operator === 'm'){
+    //   console.log(mul)
+    // }
