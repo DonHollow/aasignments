f4bda9173b4de7074bb4091030f8b64305904cbc
Yreish Pozo <yreishmiguel@gmail.com>
2022-10-11 15:37:12 -0700

    Progress

--- level_1/forception/forception.js
+++ level_1/forception/forception.js
@@ -1,21 +1,17 @@
+let newArr = []
 let people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 let alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-function forception(people, alphabet){
+function forception(){
   for (let i = 0; i < people.length; i++){
-    let peopleArr = [""]
-      console.log(" :", peopleArr)
-  }
-    // for (let i = 0; i < alphabet.length; i++)
-    //   return people.concat(alphabet);
+    newArr.push(people[i])
+    for (let j = 0; j < alphabet.length; j++){
+      newArr.push(alphabet[j].toUpperCase())
+      }
+  } return newArr
 }
 
-// forception(first, second);
-
-
-
-
-
+ console.log(forception())
 
 // Output:
 // ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
