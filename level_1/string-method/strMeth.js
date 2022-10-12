85a7b83c94f826c836a47c30b3d01ce1539e4208
Yreish Pozo <yreishmiguel@gmail.com>
2022-10-12 13:32:06 -0700

    Progress

---
+++ level_1/string-method/strMeth.js
@@ -0,0 +1,39 @@
+//* 1
+// let str = "He";
+// let capStr = str.toUpperCase()
+// let lowStr = str.toLowerCase()
+// let joined = capStr.concat(lowStr)
+
+// function joinedStr(){
+//   return joined
+// }
+
+// console.log(joinedStr())
+
+//* 2
+// let str = ("Hello");
+
+// function findMidIndex(){
+//   for (let i = 0; i <= str.length; i++){
+//     let halfStr = str.length / 2
+//     return (Math.floor(halfStr))
+//   }
+// }
+
+// console.log(findMidIndex())
+
+//* 3
+
+// let str = ("Hello World");
+
+// function returnfirstHalf(){
+//   for (let i = 0; i < str.length; i++){
+//     let halfStr = str.slice(0,5)
+//     return (halfStr)
+//   }
+// }
+
+// console.log(returnfirstHalf())
+
+//* 4
+
